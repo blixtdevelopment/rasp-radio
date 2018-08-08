2aa756e33313827a13783bab8de69b403cac9973
078emil <078emil@gmail.com>
2018-08-08 19:52:08 +0200

    New internet check, comments and update README.md
    + removed unnecesary imports and code

--- README.md
+++ README.md
@@ -1,1 +1,107 @@
-TODO
+# RASP-RADIO
+A Python program meant to control net-radio on the Raspberry Pi, located in an vintage radio (or any radio).
+
+## Getting Started
+In order to get this working on your own system, follow the steps.
+
+### Prerequisites
+Both hardware and software is needed for this so the prerequisites for both is listed.
+#### Hardware
+* Raspberry Pi, with latest Raspbian OS 
+* A old radio (or alike) with speakers and rotary switch knob (or other buttons)
+* Audio Amplifier
+* Power cables, sound cables, jumper wires
+
+##### Pin Connections
+The rotary switch knob is connected to Pin 1 (3v3)
+The different channels is connected to the following pins:
+* Pin 29 (GPIO 5)
+* Pin 31 (GPIO 6)
+* Pin 33 (GPIO 13)
+* Pin 35 (GPIO 19)
+* Pin 37 (GPIO 26)
+* Pin 36 (GPIO 16)
+
+#### Software
+The following software Prerequisites apply:
+* Python 2.7
+* MPD/MPC for controlling the channels ([See this](https://github.com/sindresorhus/awesome/blob/master/awesome.md)) 
+* The latest version of RPi.GPIO
+
+### Installing
+The program work as is, however, for it to work on boot changes to Raspbian need to made
+Adding the following to `/etc/rc.local`:
+```
+/usr/bin/python /full/path/to/file/radio.py
+```
+Now on boot rasp-radio should run be default. 
+
+## Deployment
+Out of the box it comes with 6 net-radio stations (ANR, Soft Radio, P3, Nordjyske, Nova, ABC) which are all danish
+radios. Therefore if one need to change them, the following steps is needed  (all in `radio.py`):
+
+### Find Stream
+First the stream for the net radio should be found. This is different for site to site.
+However, it can normally be found in the net-radios sites HTML or associated JavaScript. An example of this:
+ ```
+$(document).ready(function () {
+    $("#jquery_jplayer_1").jPlayer({
+        ready: function (event) {
+            $(this).jPlayer("setMedia", {
+                mp3: "http://stream.anr.dk:80/anr"
+            }).jPlayer("play");
+        },
+        swfPath: "/scripts/jplayer/jplayer",
+        supplied: "mp3",
+        wmode: "window",
+        useStateClassSkin: true
+    });
+});
+        
+```
+ Where `http://stream.anr.dk:80/anr` is the official stream.
+ 
+### Add to Rasp-Radio
+In `radio.py` add the name of the channel, stream url and pin number to switch to it. 
+Example could be Name: __Radio 123__, Url: __http://stream.radio123.com__, Pin: __GPIO 21__
+
+Add it to `CHNL_INPUT_DICT` as `CHNL_RADIO_123: 21`
+
+Add stream to `RADIO_DICT["RADIO_123"] = 'http://stream.radio123.com'`
+
+Create a radio function as follows:
+```
+def radio_123():
+    global radio_process, CHNL_CURRENT
+    try:
+        print("\nPlaying Radio 123!")
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
+        time.sleep(0.1)
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("RADIO_123") + 1)])
+        CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_RADIO_123"]
+    except Exception as er:
+        print("Error in playing Nordjyske; error: " + str(er))
+        time.sleep(2)
+        radio_123()
+```
+To `channel_selection` add:
+```
+        elif _chnl == "CHNL_RADIO_123":
+            radio_123()
+```
+
+And at last an interrupt to the selected pin:
+```
+GPIO.add_event_detect(21, GPIO.RISING, callback=callback, bouncetime=BOUNCE_TIME)
+```
+## Author
+* Emil Blixt Hansen - [Blixt Development](http://blixtdevelopment.com/) :zap:
+
+## License 
+This project is licensed under the MIT License!
+
+## Acknowledgments
+Doing development of this project, it was not planned to be shared.
+Therefore, aspects like channel adding is kinda hard-coded... It was only meant to serve as a radio at my own house.
+* Any contributions/questions/etc. are welcome
+* The code does not come with any warranty, it is provided as-is. 
--- radio.py
+++ radio.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python
 import subprocess
 import time
-import urllib
 import httplib
 import os
 import sys
-import json
 import socket
 import RPi.GPIO as GPIO
 from collections import OrderedDict, defaultdict
 
+# Setup GPIO and global variables
 GPIO.setmode(GPIO.BCM)
 BOUNCE_TIME = 900
 CHNL_CURRENT = None
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+radio_process = None
 
 # Initialise channels GPIO input
+# Could be made into an enum
 CHNL_INPUT_DICT = {"CHNL_ANR": 5,
                    "CHNL_RADIO_SOFT": 6,
                    "CHNL_P3": 13,
                    "CHNL_NORDJYSKE": 19,
                    "CHNL_NOVA": 26,
                    "CHNL_ABC": 16}
+GPIO.setup(CHNL_INPUT_DICT.values(), GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # Create the ordered dictionary, such that the radio number will be consistent
 RADIO_DICT = OrderedDict()
@@ -32,9 +34,23 @@ RADIO_DICT["NORDJYSKE"] = 'http://stream.anr.dk/nordjyske'
 RADIO_DICT["NOVA"] = 'http://stream.novafm.dk/nova128'  # 'http://edge-bauerdk-02-gos1.sharp-stream.com/nova_dk_mp3'
 RADIO_DICT["ABC"] = 'http://89.249.7.68/abc'
 
-GPIO.setup(CHNL_INPUT_DICT.values(), GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-radio_process = None
 
+def is_connected():
+    hostname = 'www.google.com'
+    try:
+        # see if we can resolve the host name -- tells us if there is
+        # a DNS listening
+        host = socket.gethostbyname(hostname)
+        # connect to the host -- tells us if the host is actually
+        # reachable
+        s = socket.create_connection((host, 80), 2)
+        return True
+    except:
+        pass
+    return False
+
+
+# Deprecated because of instability --> use id_connected()
 def internet_on():
     conn = httplib.HTTPConnection("www.google.com")
     try:
@@ -43,7 +59,8 @@ def internet_on():
     except httplib.HTTPException:
         return False
     
-    
+
+# Add all of the channels listed in RADIO_DICT
 def add_player():
     global radio_process
     print("Initialising radio channels")
@@ -51,17 +68,30 @@ def add_player():
     if radio_process != None:
         print("radio_process already exist; terminating the process and restarting...")
         radio_process.terminate()
-    
+
     subprocess.Popen(["/usr/bin/mpc", "clear"])
+    time.sleep(0.05)
+    _error_adding_channels = False
     for radio, url in RADIO_DICT.iteritems():
-        print("Adding channel: " + radio + " --- at url: " + url)
-        subprocess.Popen(["/usr/bin/mpc", "add", url])
-        time.sleep(0.05)
-    print("\nAll channels added to mpc!")
-    print("The playlist is:")
-    subprocess.Popen(["/usr/bin/mpc", "playlist"])
+        try:
+            print("Adding channel: " + radio + " --- at url: " + url)
+            subprocess.Popen(["/usr/bin/mpc", "add", url])
+            time.sleep(0.1)
+        except Exception as er:
+            print("Unable to add channel " + radio + " --- at url: " + url)
+            print("Error: " + str(er))
+            _error_adding_channels = True
+    if not _error_adding_channels:
+        print("\nAll channels added to mpc!")
+        print("The playlist is:")
+        subprocess.Popen(["/usr/bin/mpc", "playlist"])
+    else:
+        print("Not all channels was added successfully...")
+        print("Retrying!")
+        time.sleep(2)
+        add_player()
 
-
+# Find out which pin input is active on boot
 def init_current_channel():
     for channel in CHNL_INPUT_DICT.values():
         if GPIO.input(channel):
@@ -71,66 +101,102 @@ def init_current_channel():
     anr()
 
 
+# The following functions is each radio station, were the following happens:
+#       Stop the current played channel
+#       Play the new channel
+#       Update the global variable, keeping track of which channel is currently active.
+# TODO: Could potentially be made into its own class for "better programming ethics"
 def anr():
     global radio_process, CHNL_CURRENT
     print("\nPlaying ANR!")
-    radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
-    subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/anr.wav"])
-    time.sleep(0.1)
-    radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("ANR") + 1)])
-    CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_ANR"]
+    try:
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
+        #subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/anr.wav"])
+        time.sleep(0.1)
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("ANR") + 1)])
+        CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_ANR"]
+    except Exception as er:
+        print("Error in playing ANR; error: " + str(er))
+        time.sleep(2)
+        anr()
 
 
 def radio_soft():
     global radio_process, CHNL_CURRENT
-    print("\nPlaying Radio Soft!")
-    radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
-    subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/radio_soft.wav"])
-    time.sleep(0.1)
-    radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("RADIO_SOFT") + 1)])
-    CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_RADIO_SOFT"]
+    try:
+        print("\nPlaying Radio Soft!")
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
+        #subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/radio_soft.wav"])
+        time.sleep(0.1)
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("RADIO_SOFT") + 1)])
+        CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_RADIO_SOFT"]
+    except Exception as er:
+        print("Error in playing Radio Soft; error: " + str(er))
+        time.sleep(2)
+        radio_soft()
 
 
 def p3():
     global radio_process, CHNL_CURRENT
-    print("\nPlaying P3!")
-    radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
-    subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/p3.wav"])
-    time.sleep(0.1)
-    radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("P3") + 1)])
-    CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_P3"]
+    try:
+        print("\nPlaying P3!")
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
+        #subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/p3.wav"])
+        time.sleep(0.1)
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("P3") + 1)])
+        CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_P3"]
+    except Exception as er:
+        print("Error in playing P3; error: " + str(er))
+        time.sleep(2)
+        p3()
 
 
 def nordjyske():
     global radio_process, CHNL_CURRENT
-    print("\nPlaying Nordjyske!")
-    radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
-    subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/nordjyske.wav"])
-    time.sleep(0.1)
-    radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("NORDJYSKE") + 1)])
-    CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_NORDJYSKE"]
+    try:
+        print("\nPlaying Nordjyske!")
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
+        #subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/nordjyske.wav"])
+        time.sleep(0.1)
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("NORDJYSKE") + 1)])
+        CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_NORDJYSKE"]
+    except Exception as er:
+        print("Error in playing Nordjyske; error: " + str(er))
+        time.sleep(2)
+        nordjyske()
 
 
 def nova():
     global radio_process, CHNL_CURRENT
-    print("\nPlaying NOVA!")
-    radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
-    subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/nova.wav"])
-    time.sleep(0.1)
-    radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("NOVA") + 1)])
-    CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_NOVA"]
+    try:
+        print("\nPlaying NOVA!")
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
+        #subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/nova.wav"])
+        time.sleep(0.1)
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("NOVA") + 1)])
+        CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_NOVA"]
+    except Exception as er:
+        print("Error in playing Nova; error: " + str(er))
+        time.sleep(2)
+        nova()
 
 
 def abc():
     global radio_process, CHNL_CURRENT
-    print("\nPlaying ABC!")
-    radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
-    subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/abc.wav"])
-    time.sleep(0.1)
-    radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("ABC") + 1)])
-    CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_ABC"]
+    try:
+        print("\nPlaying ABC!")
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
+        #subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/abc.wav"])
+        time.sleep(0.1)
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("ABC") + 1)])
+        CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_ABC"]
+    except Exception as er:
+        print("Error in playing ABC; error: " + str(er))
+        time.sleep(2)
+        abc()
 
 
+# Select the channel bassed on channel number (i.e. GPIO Pin number)
 def channel_selection(_chnl_number):
     try:
         _chnl = CHNL_INPUT_DICT.keys()[CHNL_INPUT_DICT.values().index(_chnl_number)]
@@ -154,23 +220,24 @@ def channel_selection(_chnl_number):
         anr()
 
 
+# Handle the interrupt by the pin number (channel)
 def callback(channel):
-    if GPIO.input(channel):
+    if GPIO.input(channel):     # if the pin is active (The probably the channel is correct)
         channel_read = 0
-        for i in range(5):
+        for i in range(5):      # If the pin is active  > 3  then it must be correct
             if GPIO.input(channel):
                 channel_read += 1
             time.sleep(0.01)
         if channel_read < 3:
             return
-        if CHNL_CURRENT != channel:
+        if CHNL_CURRENT != channel:     # Check if the channel is already active
             print("Channel is: " + str(channel))
             channel_selection(channel)
     else:
         _channel_list = []
         _value_list = []
-        for i in range(5):
-            for _channel in CHNL_INPUT_DICT.values():
+        for i in range(5):      # The pin is not active, however it might still be the right channel.
+            for _channel in CHNL_INPUT_DICT.values():   # Therefore check all pin inputs
                 _channel_list.append(_channel)
                 _value_list.append(GPIO.input(_channel))
             time.sleep(0.01)
@@ -179,17 +246,18 @@ def callback(channel):
         dd = defaultdict(int)
 
         for _channel, _state in _channel_value:
-            if _state == 1:
+            if _state == 1:                     # Only store pins that were active
                 dd[_channel] += 1
 
         max_occurrence = max(dd.iteritems(), key=lambda x: x[1])
-        if max_occurrence[1] < 3:
+        if max_occurrence[1] < 3:               # If one pin were active more > 3 then select that pin
             return
-        if CHNL_CURRENT != max_occurrence[0]:
+        if CHNL_CURRENT != max_occurrence[0]:   # Check if the channel is already active
             print("Channel is: " + str(max_occurrence[0]))
             channel_selection(max_occurrence[0])
 
 
+# Initialise the interrupts
 GPIO.add_event_detect(5, GPIO.RISING, callback=callback, bouncetime=BOUNCE_TIME)
 GPIO.add_event_detect(6, GPIO.RISING, callback=callback, bouncetime=BOUNCE_TIME)
 GPIO.add_event_detect(13, GPIO.RISING, callback=callback, bouncetime=BOUNCE_TIME)
@@ -198,14 +266,10 @@ GPIO.add_event_detect(26, GPIO.RISING, callback=callback, bouncetime=BOUNCE_TIME
 GPIO.add_event_detect(16, GPIO.RISING, callback=callback, bouncetime=BOUNCE_TIME)
 
 if __name__ == "__main__":
-    # global radio_process
-    internet = internet_on()
-    if internet:
+    if is_connected():  # Check for internet first
         add_player()
+        time.sleep(0.1)
         init_current_channel()
-        print("\n---")
-        print(json.dumps(RADIO_DICT))
-        print("---\n")
     else:
         print("No connection - trying reconnecting...")
         time.sleep(2)
