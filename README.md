c645731877eed81c53fe12b24e1b253598faac21
078emil <078emil@gmail.com>
2018-08-30 20:38:19 +0200

    Added Google Assistant SDK integration and migrated to Python 3.5
    removed radio.py

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # RASP-RADIO
 A Python program meant to control net-radio on the Raspberry Pi, located in an vintage radio (or any radio).
+Furthermore, it also have build in Google Assistant SDK if that is wanted.
 
 ## Getting Started
 In order to get this working on your own system, follow the steps.
@@ -10,6 +11,7 @@ Both hardware and software is needed for this so the prerequisites for both is l
 * Raspberry Pi, with latest Raspbian OS 
 * A old radio (or alike) with speakers and rotary switch knob (or other buttons)
 * Audio Amplifier
+* USB Microphone
 * Power cables, sound cables, jumper wires
 
 ##### Pin Connections
@@ -24,21 +26,28 @@ The different channels is connected to the following pins:
 
 #### Software
 The following software Prerequisites apply:
-* Python 2.7
-* MPD/MPC for controlling the channels ([See this](https://github.com/sindresorhus/awesome/blob/master/awesome.md)) 
+* Python 3.5
+* MPD/MPC for controlling the channels ([see here](https://github.com/sindresorhus/awesome/blob/master/awesome.md)) 
 * The latest version of RPi.GPIO
+* Google Assistant SDK (with Developer Project and Actions on Google).
+    * Follow the official guide from ([Google](https://developers.google.com/assistant/sdk/guides/library/python/embed/setup)), until `Next Steps`
 
 ### Installing
-The program work as is, however, for it to work on boot changes to Raspbian need to made
-Adding the following to `/etc/rc.local`:
-```
-/usr/bin/python /full/path/to/file/radio.py
-```
-Now on boot rasp-radio should run be default. 
+The program mostly work as-is, however, two steps needs to be taken: launch on boot and assistant files.
+
+#### Launch on boot
+The program uses the built in systemd to start on boot. Run the command `sudo systemctl enable rasp-radio.service`
+and the program should run on boot. 
+
+#### Google Assistant Files
+When setting up the Google Assistant SDK, a client_secret.json file is downloaded, this should be in the same installation directory (preferably /home/pi/).
+
+Secondly, when creating Actions on Google, a Model ID is created. This can be found on [https://console.actions.google.com/](https://console.actions.google.com/)
+under Device Registration. This Model ID should be added to a file named config.ini (use config.ini.example to get started)
 
 ## Deployment
 Out of the box it comes with 6 net-radio stations (ANR, Soft Radio, P3, Nordjyske, Nova, ABC) which are all danish
-radios. Therefore if one need to change them, the following steps is needed  (all in `radio.py`):
+radios. Therefore if one need to change them, the following steps is needed  (all in `google_assistant_radio.py`):
 
 ### Find Stream
 First the stream for the net radio should be found. This is different for site to site.
@@ -65,7 +74,7 @@ $(document).ready(function () {
 In `radio.py` add the name of the channel, stream url and pin number to switch to it. 
 Example could be Name: __Radio 123__, Url: __http://stream.radio123.com__, Pin: __GPIO 21__
 
-Add it to `CHNL_INPUT_DICT` as `CHNL_RADIO_123: 21`
+Add it to `CHNL_INPUT_DICT` as `CHNL_RADIO_123 = 21`
 
 Add stream to `RADIO_DICT["RADIO_123"] = 'http://stream.radio123.com'`
 
@@ -103,5 +112,5 @@ This project is licensed under the MIT License!
 ## Acknowledgments
 Doing development of this project, it was not planned to be shared.
 Therefore, aspects like channel adding is kinda hard-coded... It was only meant to serve as a radio at my own house.
-* Any contributions/questions/etc. are welcome
+* Any contributions/questions/etc. are welcome.
 * The code does not come with any warranty, it is provided as-is. 
--- google_assistant_radio.py
+++ google_assistant_radio.py
@@ -1,13 +1,27 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import argparse
+import os.path
+import sys
+import json
+import socket
+import http.client
 import subprocess
 import time
-import httplib
-import os
-import sys
-import socket
-import RPi.GPIO as GPIO
+import configparser
+from enum import IntEnum
 from collections import OrderedDict, defaultdict
 
+import google.oauth2.credentials
+import RPi.GPIO as GPIO
+from google.assistant.library import Assistant
+from google.assistant.library.event import EventType
+from google.assistant.library.file_helpers import existing_file
+
+
+# --- Radio Elements ---
 # Setup GPIO and global variables
 GPIO.setmode(GPIO.BCM)
 BOUNCE_TIME = 900
@@ -15,15 +29,26 @@ CHNL_CURRENT = None
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 radio_process = None
 
+
 # Initialise channels GPIO input
-# Could be made into an enum
-CHNL_INPUT_DICT = {"CHNL_ANR": 5,
-                   "CHNL_RADIO_SOFT": 6,
-                   "CHNL_P3": 13,
-                   "CHNL_NORDJYSKE": 19,
-                   "CHNL_NOVA": 26,
-                   "CHNL_ABC": 16}
-GPIO.setup(CHNL_INPUT_DICT.values(), GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# Could be made into an enum <--- this
+class CHNL_INPUT_DICT(IntEnum):
+    CHNL_ANR = 5
+    CHNL_RADIO_SOFT = 6
+    CHNL_P3 = 13
+    CHNL_NORDJYSKE = 19
+    CHNL_NOVA = 26
+    CHNL_ABC = 16
+
+    def __int__(self):
+        return self.value
+
+    @staticmethod
+    def values_to_list():
+        return [int(chnl) for chnl in CHNL_INPUT_DICT]
+
+
+GPIO.setup(CHNL_INPUT_DICT.values_to_list(), GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # Create the ordered dictionary, such that the radio number will be consistent
 RADIO_DICT = OrderedDict()
@@ -33,6 +58,38 @@ RADIO_DICT["P3"] = 'http://live-icy.gss.dr.dk/A/A05H.mp3'
 RADIO_DICT["NORDJYSKE"] = 'http://stream.anr.dk/nordjyske'
 RADIO_DICT["NOVA"] = 'http://stream.novafm.dk/nova128'  # 'http://edge-bauerdk-02-gos1.sharp-stream.com/nova_dk_mp3'
 RADIO_DICT["ABC"] = 'http://89.249.7.68/abc'
+
+
+def process_event(event):
+    """Pretty prints events.
+    Prints all events that occur with two spaces between each new
+    conversation and a single space between turns of a conversation.
+    Args:
+        event(event.Event): The current event to process.
+    """
+    global radio_process
+    if event.type == EventType.ON_CONVERSATION_TURN_STARTED:
+        try:
+            radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
+        except:
+            pass
+        print()
+
+    print(event)
+
+    if (event.type == EventType.ON_CONVERSATION_TURN_FINISHED and
+            event.args and not event.args['with_follow_on_turn']):
+        try:
+            radio_process = subprocess.Popen(["/usr/bin/mpc", "play"])
+        except:
+            pass
+        print()
+    elif (event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT):
+        try:
+            radio_process = subprocess.Popen(["/usr/bin/mpc", "play"])
+        except:
+            pass
+        print()
 
 
 def is_connected():
@@ -52,13 +109,13 @@ def is_connected():
 
 # Deprecated because of instability --> use id_connected()
 def internet_on():
-    conn = httplib.HTTPConnection("www.google.com")
+    conn = http.client.HTTPConnection("www.google.com")
     try:
         conn.request("HEAD", "/")
         return True
-    except httplib.HTTPException:
+    except http.client.HTTPException:
         return False
-    
+
 
 # Add all of the channels listed in RADIO_DICT
 def add_player():
@@ -72,7 +129,7 @@ def add_player():
     subprocess.Popen(["/usr/bin/mpc", "clear"])
     time.sleep(0.05)
     _error_adding_channels = False
-    for radio, url in RADIO_DICT.iteritems():
+    for radio, url in RADIO_DICT.items():
         try:
             print("Adding channel: " + radio + " --- at url: " + url)
             subprocess.Popen(["/usr/bin/mpc", "add", url])
@@ -91,9 +148,10 @@ def add_player():
         time.sleep(2)
         add_player()
 
+
 # Find out which pin input is active on boot
 def init_current_channel():
-    for channel in CHNL_INPUT_DICT.values():
+    for channel in CHNL_INPUT_DICT:
         if GPIO.input(channel):
             channel_selection(channel)
             return
@@ -113,8 +171,8 @@ def anr():
         radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
         #subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/anr.wav"])
         time.sleep(0.1)
-        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("ANR") + 1)])
-        CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_ANR"]
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(list(RADIO_DICT).index("ANR") + 1)])
+        CHNL_CURRENT = CHNL_INPUT_DICT.CHNL_ANR
     except Exception as er:
         print("Error in playing ANR; error: " + str(er))
         time.sleep(2)
@@ -128,8 +186,8 @@ def radio_soft():
         radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
         #subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/radio_soft.wav"])
         time.sleep(0.1)
-        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("RADIO_SOFT") + 1)])
-        CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_RADIO_SOFT"]
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(list(RADIO_DICT).index("RADIO_SOFT") + 1)])
+        CHNL_CURRENT = CHNL_INPUT_DICT.CHNL_RADIO_SOFT
     except Exception as er:
         print("Error in playing Radio Soft; error: " + str(er))
         time.sleep(2)
@@ -143,8 +201,8 @@ def p3():
         radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
         #subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/p3.wav"])
         time.sleep(0.1)
-        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("P3") + 1)])
-        CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_P3"]
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(list(RADIO_DICT).index("P3") + 1)])
+        CHNL_CURRENT = CHNL_INPUT_DICT.CHNL_P3
     except Exception as er:
         print("Error in playing P3; error: " + str(er))
         time.sleep(2)
@@ -158,8 +216,8 @@ def nordjyske():
         radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
         #subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/nordjyske.wav"])
         time.sleep(0.1)
-        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("NORDJYSKE") + 1)])
-        CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_NORDJYSKE"]
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(list(RADIO_DICT).index("NORDJYSKE") + 1)])
+        CHNL_CURRENT = CHNL_INPUT_DICT.CHNL_NORDJYSKE
     except Exception as er:
         print("Error in playing Nordjyske; error: " + str(er))
         time.sleep(2)
@@ -173,8 +231,8 @@ def nova():
         radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
         #subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/nova.wav"])
         time.sleep(0.1)
-        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("NOVA") + 1)])
-        CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_NOVA"]
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(list(RADIO_DICT).index("NOVA") + 1)])
+        CHNL_CURRENT = CHNL_INPUT_DICT.CHNL_NOVA
     except Exception as er:
         print("Error in playing Nova; error: " + str(er))
         time.sleep(2)
@@ -188,29 +246,29 @@ def abc():
         radio_process = subprocess.Popen(["/usr/bin/mpc", "stop"])
         #subprocess.call(["/usr/bin/aplay", CURRENT_DIR + "/channel_name/abc.wav"])
         time.sleep(0.1)
-        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(RADIO_DICT.keys().index("ABC") + 1)])
-        CHNL_CURRENT = CHNL_INPUT_DICT["CHNL_ABC"]
+        radio_process = subprocess.Popen(["/usr/bin/mpc", "play", str(list(RADIO_DICT).index("ABC") + 1)])
+        CHNL_CURRENT = CHNL_INPUT_DICT.CHNL_ABC
     except Exception as er:
         print("Error in playing ABC; error: " + str(er))
         time.sleep(2)
         abc()
 
 
-# Select the channel bassed on channel number (i.e. GPIO Pin number)
+# Select the channel based on channel number (i.e. GPIO Pin number)
 def channel_selection(_chnl_number):
     try:
-        _chnl = CHNL_INPUT_DICT.keys()[CHNL_INPUT_DICT.values().index(_chnl_number)]
-        if _chnl == "CHNL_ANR":
+        #_chnl = CHNL_INPUT_DICT.keys()[CHNL_INPUT_DICT.values().index(_chnl_number)]
+        if _chnl_number == CHNL_INPUT_DICT.CHNL_ANR:
             anr()
-        elif _chnl == "CHNL_RADIO_SOFT":
+        elif _chnl_number == CHNL_INPUT_DICT.CHNL_RADIO_SOFT:
             radio_soft()
-        elif _chnl == "CHNL_P3":
+        elif _chnl_number == CHNL_INPUT_DICT.CHNL_P3:
             p3()
-        elif _chnl == "CHNL_NORDJYSKE":
+        elif _chnl_number == CHNL_INPUT_DICT.CHNL_NORDJYSKE:
             nordjyske()
-        elif _chnl == "CHNL_NOVA":
+        elif _chnl_number == CHNL_INPUT_DICT.CHNL_NOVA:
             nova()
-        elif _chnl == "CHNL_ABC":
+        elif _chnl_number == CHNL_INPUT_DICT.CHNL_ABC:
             abc()
         else:  # Default if other fails
             print("Unknown channel number in channel selection, selecting default (anr)!")
@@ -237,7 +295,7 @@ def callback(channel):
         _channel_list = []
         _value_list = []
         for i in range(5):      # The pin is not active, however it might still be the right channel.
-            for _channel in CHNL_INPUT_DICT.values():   # Therefore check all pin inputs
+            for _channel in CHNL_INPUT_DICT.values_to_list():   # Therefore check all pin inputs
                 _channel_list.append(_channel)
                 _value_list.append(GPIO.input(_channel))
             time.sleep(0.01)
@@ -249,7 +307,7 @@ def callback(channel):
             if _state == 1:                     # Only store pins that were active
                 dd[_channel] += 1
 
-        max_occurrence = max(dd.iteritems(), key=lambda x: x[1])
+        max_occurrence = max(dd.items(), key=lambda x: x[1])
         if max_occurrence[1] < 3:               # If one pin were active more > 3 then select that pin
             return
         if CHNL_CURRENT != max_occurrence[0]:   # Check if the channel is already active
@@ -265,7 +323,38 @@ GPIO.add_event_detect(19, GPIO.RISING, callback=callback, bouncetime=BOUNCE_TIME
 GPIO.add_event_detect(26, GPIO.RISING, callback=callback, bouncetime=BOUNCE_TIME)
 GPIO.add_event_detect(16, GPIO.RISING, callback=callback, bouncetime=BOUNCE_TIME)
 
-if __name__ == "__main__":
+
+def main(device_model):
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--credentials', type=existing_file,
+                        metavar='OAUTH2_CREDENTIALS_FILE',
+                        default=os.path.join(
+                            os.path.expanduser('/home/pi/.config'),
+                            'google-oauthlib-tool',
+                            'credentials.json'
+                        ),
+                        help='Path to store and read OAuth2 credentials')
+    args = parser.parse_args()
+    print(args)
+    with open(args.credentials, 'r') as f:
+        credentials = google.oauth2.credentials.Credentials(token=None,
+                                                            **json.load(f))
+    try:
+        with Assistant(credentials, device_model) as assistant:
+            for event in assistant.start():
+                process_event(event)
+    except Exception as e:
+        # Keyboard interrupt or other stuff that causes termination -
+        # clean up any running child process.
+        radio_process.terminate()
+        GPIO.cleanup()
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
+
+
+if __name__ == '__main__':
+    print("Starting radio service...")
     if is_connected():  # Check for internet first
         add_player()
         time.sleep(0.1)
@@ -275,12 +364,27 @@ if __name__ == "__main__":
         time.sleep(2)
         python = sys.executable
         os.execl(python, python, *sys.argv)
-    try:
-        while True:
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    device_model_id = config['DEFAULT']['DEVICE_MODEL_ID']
+    use_assistant = config['DEFAULT'].getboolean('USE_GOOGLE_ASSISTANT')
+
+    if use_assistant:
+        print("Starting google assistant service in 2 sec...")
+        time.sleep(2)
+        try:
+            main(device_model_id)
+            # while(True):
+            #     time.sleep(0.1)
+        except Exception as er:
+            print("Not able to start Google Assistant: " + er)
+            #time.sleep(2)
+            #python = sys.executable
+            #os.execl(python, python, *sys.argv)
+    else:
+        try:
+            while True:
                 time.sleep(0.1)
-    except Exception as e:
-        # Keyboard interrupt or other stuff that causes termination -
-        # clean up any running child process.
-        if radio_process != None:
-                radio_process.terminate()
-                GPIO.cleanup()
+        except Exception as er:
+            print("Exiting rasp-radio...")
